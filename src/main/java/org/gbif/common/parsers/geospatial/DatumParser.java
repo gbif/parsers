8d4c329f39f2e337513e964d0140feeb4cf93b42
Markus Döring <mdoering@gbif.org>
2014-05-21 00:17:15 +0200

    Adding a new geodetic datum parser

---
+++ src/main/java/org/gbif/common/parsers/geospatial/DatumParser.java
@@ -0,0 +1,75 @@
+package org.gbif.common.parsers.geospatial;
+
+import org.gbif.common.parsers.core.ASCIIParser;
+import org.gbif.common.parsers.core.FileBasedDictionaryParser;
+import org.gbif.common.parsers.core.ParseResult;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.base.Strings;
+
+/**
+ * Parser for geodetic datum strings into EPSG integer codes.
+ * These codes are suitable for instantiating spatial reference systems (SRS) in spatial libraries like geotools.
+ * See <a href="http://docs.geotools.org/latest/userguide/library/referencing/epsg.html">EPSG on Geotools</a>.
+ *
+ * For mapping common names to EPSG codes use the code registry search here:
+ * <ul>
+ *   <li>http://georepository.com/search/by-name/?query=samoa</li>
+ *   <li>http://www.epsg-registry.org/</li>
+ * </ul>
+ *
+ */
+public class DatumParser extends FileBasedDictionaryParser<Integer> {
+  private static DatumParser singletonObject;
+  private final Pattern EPSG = Pattern.compile("\\s*(EPSG|ESPG)\\s*:+\\s*(\\d+)\\s*$", Pattern.CASE_INSENSITIVE);
+  private final Pattern NORMALIZER = Pattern.compile("[^\\w:]+");
+  private final ASCIIParser ascii = ASCIIParser.getInstance();
+
+  private DatumParser() {
+    super(false);
+    init(DatumParser.class.getResourceAsStream("/dictionaries/parse/datum.txt"));
+  }
+
+  public static DatumParser getInstance() {
+    synchronized (DatumParser.class) {
+      if (singletonObject == null) {
+        singletonObject = new DatumParser();
+      }
+    }
+    return singletonObject;
+  }
+
+  @Override
+  public ParseResult<Integer> parse(String input) {
+    if (Strings.isNullOrEmpty(input)) {
+      return null;
+    }
+    // try EPSG codes directly, allow common typo
+    Matcher m = EPSG.matcher(input);
+    if (m.find()) {
+      Integer code = Integer.valueOf(m.group(2));
+      return ParseResult.success(ParseResult.CONFIDENCE.DEFINITE, code);
+    }
+    // try dictionary for well known SRS names
+    return super.parse(input);
+  }
+
+  @Override
+  protected String normalize(String value) {
+    if (Strings.isNullOrEmpty(value)) return null;
+    // convert to ascii
+    ParseResult<String> asci = ascii.parse(value);
+    return NORMALIZER.matcher(asci.getPayload()).replaceAll("").toUpperCase();
+  }
+
+  @Override
+  protected Integer fromDictFile(String value) {
+    if (Strings.isNullOrEmpty(value)) {
+      return null;
+    }
+    return Integer.valueOf(value);
+  }
+
+}
---
+++ src/test/java/org/gbif/common/parsers/geospatial/DatumParserTest.java
@@ -0,0 +1,32 @@
+package org.gbif.common.parsers.geospatial;
+
+import org.gbif.common.parsers.ParserTestBase;
+
+import org.junit.Test;
+
+/**
+ *
+ */
+public class DatumParserTest extends ParserTestBase<Integer> {
+
+  public DatumParserTest() {
+    super(DatumParser.getInstance());
+  }
+
+  @Test
+  public void testParse() {
+    // run a few basic tests to check it bootstraps and appears to work
+    assertParseSuccess(4269, "EPSG::4269");
+    assertParseSuccess(6317, " EPSG::6317");
+    assertParseSuccess(4269, "EPSG:4269");
+    assertParseSuccess(4269, " EPSG :: 4269 ");
+    assertParseSuccess(4269, "ESPG:4269");
+    // known srs names
+    assertParseSuccess(6269, "NAD83");
+    assertParseSuccess(4326, "WGS84");
+    assertParseSuccess(6269, "nad83");
+    assertParseSuccess(6301, "tokYo!");
+
+  }
+
+}
