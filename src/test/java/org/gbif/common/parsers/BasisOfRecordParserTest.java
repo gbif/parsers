a967bb93f77a230d07c6b99aa2068a38b71e9fdf
Markus Döring <mdoering@gbif.org>
2014-05-29 12:17:41 +0200

    Added material sample to BoR parser and adding more tests, updating the mappings too

--- src/test/java/org/gbif/common/parsers/BasisOfRecordParserTest.java
+++ src/test/java/org/gbif/common/parsers/BasisOfRecordParserTest.java
@@ -1,8 +1,18 @@
 package org.gbif.common.parsers;
 
 import org.gbif.api.vocabulary.BasisOfRecord;
+import org.gbif.common.parsers.core.ParseResult;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.Sets;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -49,5 +59,49 @@ public class BasisOfRecordParserTest extends ParserTestBase<BasisOfRecord> {
     assertParseSuccess(BasisOfRecord.LIVING_SPECIMEN, "germplasm");
     assertParseSuccess(BasisOfRecord.FOSSIL_SPECIMEN, "fossil");
     assertParseSuccess(BasisOfRecord.LITERATURE, "literature");
+
+    assertParseSuccess(BasisOfRecord.PRESERVED_SPECIMEN, "50 specimens");
+    assertParseSuccess(BasisOfRecord.PRESERVED_SPECIMEN, "1250 specimens");
+    assertParseSuccess(BasisOfRecord.PRESERVED_SPECIMEN, "72 specimens");
+    assertParseSuccess(BasisOfRecord.PRESERVED_SPECIMEN, "Espèce");
+    assertParseSuccess(BasisOfRecord.LIVING_SPECIMEN, "living, growing plant");
+    assertParseSuccess(BasisOfRecord.LIVING_SPECIMEN, "germplasm");
+    assertParseSuccess(BasisOfRecord.PRESERVED_SPECIMEN, "Alcohol / Microscope");
+    assertParseSuccess(BasisOfRecord.PRESERVED_SPECIMEN, "preservé 179600");
+  }
+
+
+  /**
+   * Parse all unique basis of record values found in our index and make sure parsing doesn't get worse.
+   * If the test file is updated, values here need to be adjusted!
+   */
+  @Test
+  public void testOccurrenceValues() throws IOException {
+    final int CURRENT_TESTS_SUCCESSFUL = 248;
+    final int CURRENT_DISTINCT = 9;
+
+    int failed = 0;
+    int success = 0;
+    Set<BasisOfRecord> values = Sets.newHashSet();
+
+    BufferedReader r = new BufferedReader(new InputStreamReader(
+      getClass().getResourceAsStream("/parse/basisofrecord/test_bor.txt"), Charsets.UTF_8));
+    String line;
+    while ((line=r.readLine()) != null) {
+      ParseResult<BasisOfRecord> parsed = parser.parse(line);
+      if (parsed.isSuccessful()) {
+        success++;
+        values.add(parsed.getPayload());
+      } else {
+        System.out.println("Failed: " + line);
+        failed++;
+      }
+    }
+
+    System.out.println(failed + " failed parse results");
+    System.out.println(success + " successful parse results");
+    System.out.println(values.size() + " distinct basis of record values parsed");
+    assertTrue(CURRENT_TESTS_SUCCESSFUL <= success);
+    assertTrue(CURRENT_DISTINCT <= values.size());
   }
 }
